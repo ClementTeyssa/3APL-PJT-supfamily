8388f0c21df363c5776cce3e7db3a7f8ba492d6a
Clément Teyssandier <clement.teyssandier@supinfo.com>
2018-11-13 14:58:34 +0100

    Update playgrounds/MyPlayground.playground/Contents.swift

--- playgrounds/MyPlayground.playground/Contents.swift
+++ playgrounds/MyPlayground.playground/Contents.swift
@@ -3,10 +3,43 @@
 import Foundation
 import PlaygroundSupport
 
-    let url = "http://supinfo.steve-colinet.fr/supfamily/?action=login&username=admin&password=admin"
+//    let url = "http://supinfo.steve-colinet.fr/supfamily/?action=login&username=admin&password=admin"
+//
+//let request = NSMutableURLRequest(url: URL(string: url)!)
+//request.httpMethod = "GET"
+//
+//let requestAPI = URLSession.shared.dataTask(with: request as URLRequest) {data, response, error in
+//    if (error != nil) {
+//        print(error!.localizedDescription) // On indique dans la console ou est le problème dans la requête
+//    }
+//    if let httpStatus = response as? HTTPURLResponse , httpStatus.statusCode != 200 {
+//        print("statusCode devrait être de 200, mais il est de \(httpStatus.statusCode)")
+//        print("réponse = \(response)") // On affiche dans la console si le serveur ne nous renvoit pas un code de 200 qui est le code normal
+//    }
+//
+//    let responseAPI = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+//    print("responseString = \(responseAPI)") // Affiche dans la console la réponse de l'API
+//
+//    if error == nil {
+//        //
+//    }
+//}
+//requestAPI.resume()
+
+
+
+
+
+
+
+
+let url = "http://supinfo.steve-colinet.fr/supfamily/"
 
 let request = NSMutableURLRequest(url: URL(string: url)!)
-request.httpMethod = "GET"
+request.httpMethod = "POST"
+
+let postString = "action=login&username=admin&password=admin"
+request.httpBody = postString.data(using: String.Encoding.utf8)
 
 let requestAPI = URLSession.shared.dataTask(with: request as URLRequest) {data, response, error in
     if (error != nil) {
@@ -21,8 +54,18 @@ let requestAPI = URLSession.shared.dataTask(with: request as URLRequest) {data, 
     print("responseString = \(responseAPI)") // Affiche dans la console la réponse de l'API
     
     if error == nil {
-        //
+        // Ce que vous voulez faire.
     }
 }
 requestAPI.resume()
+
+
+
+
+
+
+
+
+
+
 PlaygroundPage.current.needsIndefiniteExecution = true
