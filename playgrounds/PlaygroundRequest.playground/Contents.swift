af4a898f27524b033abc14972d2687817d53eba2
Clément Teyssandier <clement.teyssandier@supinfo.com>
2018-11-13 17:46:17 +0100

    Update playgrounds/PlaygroundRequest.playground/Contents.swift

--- playgrounds/PlaygroundRequest.playground/Contents.swift
+++ playgrounds/PlaygroundRequest.playground/Contents.swift
@@ -2,6 +2,7 @@
 
 import Foundation
 
+
 let url = "http://supinfo.steve-colinet.fr/supfamily/"
 
 let request = NSMutableURLRequest(url: URL(string: url)!)
@@ -11,13 +12,12 @@ let postString = "action=login&username=admin&password=admin"
 request.httpBody = postString.data(using: String.Encoding.utf8)
 
 let requestAPI = URLSession.shared.dataTask(with: request as URLRequest) {data, response, error in
-    if (error != nil) {
-        print(error!.localizedDescription) // On indique dans la console ou est le problème dans la requête
-    }
     let responseAPI = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
     print("responseString = \(responseAPI)") // Affiche dans la console la réponse de l'API
     if error == nil {
         // Ce que vous voulez faire.
+    } else {
+        print(error!.localizedDescription) // On indique dans la console ou est le problème dans la requête
     }
 }
 requestAPI.resume()
