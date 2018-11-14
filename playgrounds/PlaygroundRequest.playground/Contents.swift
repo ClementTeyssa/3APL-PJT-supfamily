57c824032129a278e63316d27dce6155bbe66abe
Clément Teyssandier <clement.teyssandier@supinfo.com>
2018-11-14 16:32:27 +0100

    Update playgrounds/PlaygroundRequest.playground/Contents.swift

--- playgrounds/PlaygroundRequest.playground/Contents.swift
+++ playgrounds/PlaygroundRequest.playground/Contents.swift
@@ -5,19 +5,21 @@ func apiRequest(toPost:String) -> Data{
     let request = NSMutableURLRequest(url: URL(string: url)!)
     request.httpMethod = "POST"
     request.httpBody = toPost.data(using: String.Encoding.utf8)
-    
+    var dataRes:Data = .init()
     let requestAPI = URLSession.shared.dataTask(with: request as URLRequest) {data, response, error in
         if error == nil {
-            return data!
+            dataRes = data!
         } else {
-            return nil
+//            return nil
         }
     }
     requestAPI.resume()
     sleep(1)
+    return dataRes
 }
 
 let postString = "action=login&username=admin&password=admin"
+let data = apiRequest(toPost: postString)
 let response = apiRequest(toPost: postString)
 let responseString = NSString(data: response, encoding: String.Encoding.utf8.rawValue)
 print(responseString)
