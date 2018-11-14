0a1719cb5ec2dc2237a9c806a6b9741afa9d259e
Clément Teyssandier <clement.teyssandier@supinfo.com>
2018-11-14 10:22:25 +0100

    Update playgrounds/PlaygroundRequest.playground/Contents.swift

--- playgrounds/PlaygroundRequest.playground/Contents.swift
+++ playgrounds/PlaygroundRequest.playground/Contents.swift
@@ -1,25 +1,23 @@
-//: Playground - noun: a place where people can play
-
 import Foundation
 
-
-let url = "http://supinfo.steve-colinet.fr/supfamily/"
-
-let request = NSMutableURLRequest(url: URL(string: url)!)
-request.httpMethod = "POST"
+func apiRequest(toPost:String) -> Data{
+    let url = "http://supinfo.steve-colinet.fr/supfamily/"
+    let request = NSMutableURLRequest(url: URL(string: url)!)
+    request.httpMethod = "POST"
+    request.httpBody = toPost.data(using: String.Encoding.utf8)
+    
+    let requestAPI = URLSession.shared.dataTask(with: request as URLRequest) {data, response, error in
+        if error == nil {
+            return data!
+        } else {
+            return nil
+        }
+    }
+    requestAPI.resume()
+    sleep(1)
+}
 
 let postString = "action=login&username=admin&password=admin"
-request.httpBody = postString.data(using: String.Encoding.utf8)
-
-let requestAPI = URLSession.shared.dataTask(with: request as URLRequest) {data, response, error in
-    let responseAPI = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
-    print("responseString = \(responseAPI)") // Affiche dans la console la réponse de l'API
-    if error == nil {
-        // Ce que vous voulez faire.
-    } else {
-        print(error!.localizedDescription) // On indique dans la console ou est le problème dans la requête
-    }
-}
-requestAPI.resume()
-
-sleep(1)
+let response = apiRequest(toPost: postString)
+let responseString = NSString(data: response, encoding: String.Encoding.utf8.rawValue)
+print(responseString)
